ad68285405238dbde66c1cc0db072974c22eb89d
Heo-PC\Heo <hjs810326@incross.com>
2019-04-29 11:33:01 +0900

    미디에이션 지원 중인 광고 플랫폼 SDK 최신화

--- adlibrTestProject/app/src/main/java/test/adlib/project/ads/SubAdlibAdViewMezzo.java
+++ adlibrTestProject/app/src/main/java/test/adlib/project/ads/SubAdlibAdViewMezzo.java
@@ -4,19 +4,15 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.mapps.android.listner.AdListner;
 import com.mapps.android.view.AdView;
+import com.mz.common.listener.AdListener;
+
 import com.mocoplex.adlib.SubAdlibAdViewCore;
 
 public class SubAdlibAdViewMezzo extends SubAdlibAdViewCore {
-
     protected AdView ad = null;
-    protected boolean bPassAd = false;
 
-    // 여기에 MEZZO ID 를 입력하세요.
-    // 발급받은 지면이 mezzo/Solution/ 형식이면 mezzoID에 입력. 없으면 ""
-    protected String mezzoID = "";
-    // 발급받은 코드가 34,227,479 형식이면 순서대로 publisherCode, mediaCode, sectionCode에 입력. 없으면 ""
+    // 발급받은 코드가 34,227,479 형식이면 순서대로 publisherCode, mediaCode, sectionCode에 입력.
     protected String publisherCode = "";
     protected String mediaCode = "";
     protected String sectionCode = "";
@@ -28,39 +24,38 @@ public class SubAdlibAdViewMezzo extends SubAdlibAdViewCore {
     public SubAdlibAdViewMezzo(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        ad = new AdView(context, 1, 0, AdView.TYPE_IMAGE);
-        if(!publisherCode.equals("") && !mediaCode.equals("") && !sectionCode.equals("")) {
-            ad.setAdViewCode(publisherCode, mediaCode, sectionCode);
-        }
-        ad.setAdListner(new AdListner() {
+        ad = new AdView(context, 1, 0, AdView.TYPE_HTML);
+        ad.setAdViewCode(publisherCode, mediaCode, sectionCode);
+        ad.setAdListener(new AdListener() {
 
             @Override
             public void onChargeableBannerType(View v, boolean bcharge) {
-
                 if(ad == v){
                     // 무료광고 일 경우 다음광고로 넘깁니다.
                     if(!bcharge){
-                        bPassAd = true;
                         failed();
+                    } else {
+                        queryAd();
+                        gotAd();
                     }
                 }
             }
 
             @Override
             public void onFailedToReceive(View v, int errCode) {
-
                 if(ad == v){
-                    if(errCode == 0){
-                        // 광고 수신 성공
-                        if(!bPassAd){
-                            queryAd();
-                            gotAd();
-                        }
-                    }else{
-                        // 광고 수신 실패
+                    if (errCode != 0) {
                         failed();
                     }
                 }
+            }
+
+            @Override
+            public void onInterClose(View view) {
+            }
+
+            @Override
+            public void onAdClick(View view) {
             }
         });
     }
@@ -68,8 +63,6 @@ public class SubAdlibAdViewMezzo extends SubAdlibAdViewCore {
     // 스케줄러에의해 자동으로 호출됩니다.
     // 실제로 광고를 보여주기 위하여 요청합니다.
     public void query() {
-        bPassAd = false;
-
         this.removeAllViews();
         this.addView(ad);
 
@@ -90,7 +83,6 @@ public class SubAdlibAdViewMezzo extends SubAdlibAdViewCore {
         if(ad != null){
             ad.StartService();
         }
-
         super.onResume();
     }
 
@@ -98,17 +90,13 @@ public class SubAdlibAdViewMezzo extends SubAdlibAdViewCore {
         if(ad != null){
             ad.StopService();
         }
-
         super.onPause();
     }
 
     public void onDestroy() {
         if(ad != null){
             this.removeView(ad);
-            ad.StopService();
-            ad = null;
         }
-
         super.onDestroy();
     }
 }
