ff8cd31c0233064e20770fdc2e7cd08ada3e4e8c
Hibbah <joohyun.won@nhnpayco.com>
2020-02-14 16:51:31 +0900

    [샘플앱] facebook audience network SDK 명시적 초기화 코드 추가

---
+++ adlibrTestProject/app/src/main/java/test/adlib/project/AdlibSampleApplication.java
@@ -0,0 +1,14 @@
+package test.adlib.project;
+
+import android.support.multidex.MultiDexApplication;
+
+public class AdlibSampleApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 페이스북 AudienceNetworkSDK v5.3.0 이상을 사용하는 경우에만 명시적인 초기화 필요.
+        // 아래는 AudienceNetwork 가이드에 제시된 Helper 클래스를 사용한 예시.
+        AudienceNetworkInitializeHelper.initialize(this);
+    }
+}
